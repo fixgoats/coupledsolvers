af28b4df5aa20aeeda3475ccebcfa6f4fab1be37
Valtýr Kári Daníelsson <vkd1@hi.is>
2024-02-23 17:29:37 +0000

    seems somewhat promising

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,189 @@
-use nalgebra::{SVector, Vector2, SMatrix};
+use num::Complex;
 use rand::Rng;
 use plotters::prelude::*;
-use ode_solvers::rk4::*;
-use num::Complex;
 
+type C64 = Complex<f64>;
+// type C32 = Complex<f32>;
+
+const C0: C64 = C64{re: 0., im: 0.};
+const CI: C64 = C64{re: 0., im: 1.};
 const NSITES: usize = 1;
-const TRIPLENSITES: usize = 3 * NSITES;
-// type C64 = Complex<f64>;
-type State = SVector<f64, TRIPLENSITES>;
-/*type SimplerState = Vector2<f64>;
+const NSTEPS: usize = 10000;
+const DT: f64 = 0.005;
+const GAMMA: f64 = 0.1;
+const ALPHA: f64 = 0.0004;
+const P: f64 = 10.;
+const R: f64 = 0.016;
+const G: f64 = 0.002;
+const OMEGA: f64 = 0.1;
+//const D: [usize; NSITES*NSITES] = [0, 12, 20, 12, 0, 8, 20, 12, 0];
+//const J: [f64; NSITES*NSITES] = [0., 0.1, 0.01,  0.1, 0., 0.1, 0.01, 0.1, 0.];
+const D: [usize; 1] = [0];
+const J: [f64; 1] = [0.];
+//const BETA: [f64; NSITES*NSITES] = [0., 1., 1.,  1., 0., 1., 1., 1., 0.];
+const BETA: [f64; 1] = [0.];
+type PsiState = [C64; NSITES];
+type XState = [f64; NSITES];
+const PLOTNAME: &str = "test.png";
 
-struct FallingBall;
 
-impl ode_solvers::System<f64, SimplerState> for FallingBall {
-    fn system(&self, _t: f64, y: &SimplerState, dy: &mut SimplerState) {
-        dy[0] = y[1];
-        dy[1] = -9.8;
+fn fpsi(y: C64, ydelay: &PsiState, x: f64, j: usize) -> C64 {
+    let mut delayterm = C0;
+    for i in 0..NSITES {
+        delayterm += J[j*NSITES + i] * (CI*BETA[j*NSITES + i]).exp() * ydelay[i];
     }
-}*/
-
-struct Condensate {
-    // j: SMatrix<f64, NSITES, NSITES>,
-    // d: SMatrix<f64, NSITES, NSITES>,
-    alpha: f64,
-    omega: f64,
-    g: f64,
-    r: f64,
-    // beta: f64,
-    // v: f64,
-    gamma: f64,
-    p: f64,
+    return C64{re: 0.5 * R * x, im: -(OMEGA + G * x + ALPHA * y.norm_sqr())} * y
+        + delayterm;
 }
 
-fn norm_sqr(x: f64, y: f64) -> f64 {
-    return x * x + y * y; 
-}
-
-impl ode_solvers::System<f64, State> for Condensate {
-    fn system(&self, _x: f64, y: &State, dy: &mut State) {
-        for i in 0..NSITES {
-            let x = y[2*NSITES+i];
-            let psir = y[2*i];
-            let psii = y[2*i+1];
-            dy[2*i] = 0.5 * self.r * x * psir 
-                -(self.omega + self.g * x 
-                   + self.alpha * norm_sqr(psir, psii)) * psii;
-            dy[2*i+1] = 0.5 * self.r * x * psii
-                + (self.omega + self.g * x
-                   + self.alpha * norm_sqr(psii, psir)) * psir;
-            dy[2*NSITES+i] = -(self.gamma + self.r * norm_sqr(psir, psii)) * x + self.p;
-        }
-    }
+fn fx(x: f64, y: C64) -> f64 {
+    return -(GAMMA + ALPHA * y.norm_sqr()) * x + P;
 }
 
 #[allow(dead_code)]
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
+fn init_psi() -> PsiState {
+    let mut rng = rand::thread_rng();
+    return [(); NSITES].map(|_| C64{re: rng.gen_range(0.0..0.02), im: rng.gen_range(0.0..0.2)});
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut rng = rand::thread_rng();
-    let mut y0 = SVector::<f64, TRIPLENSITES>::zeros();
-    for i in 0..TRIPLENSITES {
-        y0[i] = rng.gen_range(0.0..2.);
+/* fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut psis = Vec::<PsiState>::with_capacity(NSTEPS);
+    let mut xs = Vec::<XState>::with_capacity(NSTEPS);
+    //psis.push(init_psi());
+    psis.push([0.1*CI; NSITES]);
+    xs.push([0.1; NSITES]);
+
+    /*let mut k1psi = [C0; NSITES];
+    let mut k2psi = [C0; NSITES];
+    let mut k3psi = [C0; NSITES];
+    let mut k4psi = [C0; NSITES];
+    let mut k1x = [0.; NSITES];
+    let mut k2x = [0.; NSITES];
+    let mut k3x = [0.; NSITES];
+    let mut k4x = [0.; NSITES];*/
+
+    for i in 1..NSTEPS {
+        let mut yi = [C0; NSITES];
+        let mut xi = [0.; NSITES];
+        for j in 0..NSITES {
+            let y = psis[i-1][j];
+            let x = xs[i-1][j];
+            let mut ydelay = [C0; NSITES];
+            for k in 0..NSITES {
+                let d = D[j*NSITES+k];
+                if i <= d || k == j {continue}
+                else {ydelay[k] = psis[i-d-1][k];}
+            }
+            let k1 = fpsi(y, &ydelay, x, j);
+            let k2 = fpsi(y + 0.5 * DT * k1, &ydelay, x, j);
+            let k3 = fpsi(y + 0.5 * DT * k2, &ydelay, x, j);
+            let k4 = fpsi(y + DT * k3, &ydelay, x, j);
+            yi[j] = y + (DT / 6.) * (k1 + 2. * k2 + 2. * k3 + k4);
+            let k1 = fx(x, y);
+            let k2 = fx(x + 0.5 * DT * k1, y);
+            let k3 = fx(x + 0.5 * DT * k2, y);
+            let k4 = fx(x + DT * k3, y);
+            xi[j] = x + (DT / 6.) * (k1 + 2. * k2 + 2. * k3 + k4);
+        }
+        psis.push(yi);
+        xs.push(xi);
     }
+    
+    //println!("{:?}", psis.iter().map(|x| x[0].norm_sqr()).collect::<Vec<f64>>());
 
-    let system = Condensate {
-        alpha: 0.01,
-        omega: 0.01,
-        g: 0.01,
-        r: 0.1,
-        gamma: 0.01,
-        p: 30.
-    };
+    let root_area = BitMapBackend::new(PLOTNAME, (1920, 1080)).into_drawing_area();
+    root_area.fill(&WHITE)?;
+    let root_area = root_area.titled("Image Title", ("sans-serif", 60))?;
 
-    /*let system = FallingBall;
-    let y0 = SimplerState::new(10.0, 0.);*/
-    let startt = 0.;
-    let endt = 2.;
-    let dt = 1e-4;
-    let nsteps = ((endt - startt) / dt) as usize;
-    let mut bleh = Rk4::new(system, startt,  y0, endt, dt);
-    let res = bleh.integrate();
-    let psisq = bleh.y_out().
+    let (upper, lower) = root_area.split_vertically(540);
 
-    let area = BitMapBackend::new("ha.png", (1024, 760)).into_drawing_area();
-    area.fill(&WHITE)?;
-    let minx = startt;
-    let maxx = endt;
-    let miny = 0.0;
-    let maxy = 0.01;
-    let mut chart = ChartBuilder::on(&area)
-        .margin(20)
-        .caption("psi?".to_string(), ("sans", 20))
-        .build_cartesian_2d((minx..maxx).step(0.001), (miny..maxy).step(0.001))?;
+    let mut cc = ChartBuilder::on(&upper)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("X", ("sans-serif", 40))
+        .build_cartesian_2d(0.0..DT*NSTEPS as f64, -0.1..150.0)?;
 
-    chart.configure_mesh()
+    cc.configure_mesh()
         .x_labels(20)
         .y_labels(10)
-        .max_light_lines(4)
+        .x_label_formatter(&|v| format!("{:.1}", v))
         .draw()?;
 
-    chart.draw_series(
-        LineSeries::new(
-            (0..nsteps).map(|i| (i as f64 * dt, norm_sqr(bleh.y_out()[i][0], bleh.y_out()[i][1]))),
-            &BLACK
-        ))?
-        .label("Line")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLACK));
-    chart.draw_series(
-        LineSeries::new(
-            (0..nsteps).map(|i| (i as f64 * dt, bleh.y_out()[i][2])),
-            &BLUE
-        ))?
-        .label("Line")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
+    cc.draw_series(LineSeries::new(
+            (0..NSTEPS).map(|i| (i as f64 * DT, xs[i][0])), &RED))?
+        .label("emmmm")
+        .legend(|(x, y)| PathElement::new(vec![(x,y), (x+20, y)], RED));
 
-    chart.configure_series_labels().border_style(BLACK).draw()?;
-    area.present().expect("Can't write to file");
+    let mut cc = ChartBuilder::on(&lower)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("|psi|^2", ("sans-serif", 40))
+        .build_cartesian_2d(0.0..DT*NSTEPS as f64, -0.1..20000.0)?;
+
+    cc.configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .x_label_formatter(&|v| format!("{:.1}", v))
+        .draw()?;
+
+    cc.draw_series(LineSeries::new(
+            (0..NSTEPS).map(|i| (i as f64 * DT, psis[i][0].norm_sqr())), &RED))?
+        .label("emmmm")
+        .legend(|(x, y)| PathElement::new(vec![(x,y), (x+20, y)], RED));
+
+    root_area.present().expect("úps");
     Ok(())
+}*/
+
+fn main() {
+    let mut psis = Vec::<C64>::with_capacity(NSTEPS);
+    let mut xs = Vec::<f64>::with_capacity(NSTEPS);
+    psis[0] = C64{re: 0.1, im: 0.03};
+    for i in 1..NSTEPS {
+        xs[i] = xs[i-1] * (-(GAMMA + R * psis[i-1].norm_sqr())*DT).exp() + P * DT;
+        psis[i] = ()
+    }
+
+    /*let root_area = BitMapBackend::new(PLOTNAME, (1920, 1080)).into_drawing_area();
+    root_area.fill(&WHITE)?;
+    let root_area = root_area.titled("Image Title", ("sans-serif", 60))?;
+
+    let (upper, lower) = root_area.split_vertically(540);
+
+    let mut cc = ChartBuilder::on(&upper)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("X", ("sans-serif", 40))
+        .build_cartesian_2d(0.0..DT*NSTEPS as f64, -0.1..150.0)?;
+
+    cc.configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .x_label_formatter(&|v| format!("{:.1}", v))
+        .draw()?;
+
+    cc.draw_series(LineSeries::new(
+            (0..NSTEPS).map(|i| (i as f64 * DT, xs[i][0])), &RED))?
+        .label("emmmm")
+        .legend(|(x, y)| PathElement::new(vec![(x,y), (x+20, y)], RED));
+
+    let mut cc = ChartBuilder::on(&lower)
+        .margin(5)
+        .set_all_label_area_size(50)
+        .caption("|psi|^2", ("sans-serif", 40))
+        .build_cartesian_2d(0.0..DT*NSTEPS as f64, -0.1..20000.0)?;
+
+    cc.configure_mesh()
+        .x_labels(20)
+        .y_labels(10)
+        .x_label_formatter(&|v| format!("{:.1}", v))
+        .draw()?;
+
+    cc.draw_series(LineSeries::new(
+            (0..NSTEPS).map(|i| (i as f64 * DT, psis[i][0].norm_sqr())), &RED))?
+        .label("emmmm")
+        .legend(|(x, y)| PathElement::new(vec![(x,y), (x+20, y)], RED));
+
+    root_area.present().expect("úps");
+    Ok(())*/
 }
